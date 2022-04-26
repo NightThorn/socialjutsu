06ac3076d14119999c3c5d5bcf5377030be4f749
greasygamerdotcom <greasygamer@greasygamer.com>
2022-04-26 12:14:57 -0500

    ajx setup

--- inc/public/post/views/pages/general.php
+++ inc/public/post/views/pages/general.php
@@ -1,3 +1,24 @@
+<script>
+	$('#idea').submit(function() {
+		$(this).ajaxSubmit({
+			type: "POST",
+			//set the data type
+			dataType: 'json',
+			data: {
+				topic: topic
+			},
+			url: 'inc/public/post/controllers/idea.php', // target element(s) to be updated with server response 
+			cache: false,
+			//check this in Firefox browser
+			success: function(response) {
+				console.log(response);
+				alert(response)
+			},
+			error: onFailRegistered
+		});
+		return false;
+	});
+</script>
 <div class="post post-create">
 
 	<?php _e($block_post_type, false) ?>
@@ -24,20 +45,21 @@
 	</div>
 
 	<?php _e($block_schedule, false) ?>
-	<form action="<?php _e(get_module_url("topic", $this)) ?>" class="actionLogin" method="post">
+	<div>
+	<form class="actionLogin" method="post">
 		<div class="form-group">
 			<input class="form-control" type="text" name="topic" placeholder="<?php _e("Enter a topic") ?>">
 		</div>
 
-		<button class="btn wimax-btn w-100" type="submit"><?php _e("Get Post Idea") ?></button>
+		<button class="btn wimax-btn w-100" id="idea" type="submit"><?php _e("Get Post Idea") ?></button>
 	</form>
-	<h3><?php echo $result; ?></h3>
 	<div class="caption m-t-15">
-		<textarea name="topicgenerator" disabled="true" class="form-control post-message" placeholder="<?php _e('Awaiting topic input') ?>"></textarea>
+		<textarea id="ideagenerator" disabled="true" class="form-control post-message" ></textarea>
 		<div class="caption-toolbar">
 			<div class="item">
 				<div class="count-word"><i class="fas fa-text-width"></i> <span>0</span></div>
 			</div>
 		</div>
+	</div>
 	</div>
 </div>
