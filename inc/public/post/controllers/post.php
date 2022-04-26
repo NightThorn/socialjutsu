feae8ce4385841823d323b8a4232201942ce31fd
greasygamerdotcom <greasygamer@greasygamer.com>
2022-04-26 11:50:03 -0500

    1st pass at openai

--- inc/public/post/controllers/post.php
+++ inc/public/post/controllers/post.php
@@ -76,6 +76,52 @@ class post extends MY_Controller {
 
 	public function block(){}
 
+	public function topic()
+	{
+		
+		$topic = post("topic");
+		
+		$url = "https://api.openai.com/v1/engines/text-davinci-002/completions";
+		$OPENAI_API_KEY = "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";
+		$data = array(
+  "prompt"=> "Write a post for" + $topic,
+  "temperature"=> 0.83,
+  "max_tokens"=> 483,
+  "top_p"=> 1,
+  "frequency_penalty"=> 0,
+  "presence_penalty"=> 2
+		);
+		$data_string = json_encode($data);
+
+		$curl = curl_init($url);
+		curl_setopt($curl, CURLOPT_HEADER, false);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt(
+			$curl,
+			CURLOPT_HTTPHEADER,
+			array("Content-type: application/json", "Authorization: Bearer $OPENAI_API_KEY")
+		);
+		curl_setopt($curl, CURLOPT_POST, true);
+		curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
+
+		$json_response = curl_exec($curl);
+
+		$status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+		if (
+			$status != 201
+		) {
+			die("Error: call to URL $url failed with status $status, response $json_response, curl_error " . curl_error($curl) . ", curl_errno " . curl_errno($curl));
+		}
+
+
+		curl_close($curl);
+
+		$response = json_decode($json_response, true);
+		return $response;
+
+	}
+
 	public function save($skip_validate = false){
 
 		$post_type = post("post_type");
--- inc/public/post/views/pages/general.php
+++ inc/public/post/views/pages/general.php
@@ -1,28 +1,43 @@
 <div class="post post-create">
-	
-	<?php _e( $block_post_type, false) ?>
+
+	<?php _e($block_post_type, false) ?>
 
 	<div class="post-content m-b-15">
-		
+
 		<div class="item-post-type" data-type="photo">
-			<?php _e( $file_manager_photo, false) ?>
+			<?php _e($file_manager_photo, false) ?>
 		</div>
 
 		<div class="item-post-type" data-type="video">
-			<?php _e( $file_manager_video, false) ?>
+			<?php _e($file_manager_video, false) ?>
 		</div>
 
 		<div class="item-post-type" data-type="link">
-			<label class="text-uppercase"><?php _e('Link url')?></label>
-			<?php _e( $block_link, false)?>
-			<label class="text-uppercase"><?php _e('Thumbnail')?></label>
-			<?php _e( $file_manager_link, false) ?>
+			<label class="text-uppercase"><?php _e('Link url') ?></label>
+			<?php _e($block_link, false) ?>
+			<label class="text-uppercase"><?php _e('Thumbnail') ?></label>
+			<?php _e($file_manager_link, false) ?>
 		</div>
 
-		<?php _e( $block_caption, false)?>
+		<?php _e($block_caption, false) ?>
 
 	</div>
 
-	<?php _e( $block_schedule, false)?>
+	<?php _e($block_schedule, false) ?>
+	<form action="<?php _e(get_module_url("topic", $this)) ?>" class="actionLogin" method="post">
+		<div class="form-group">
+			<input class="form-control" type="text" name="topic" placeholder="<?php _e("Enter a topic") ?>">
+		</div>
 
+		<button class="btn wimax-btn w-100" type="submit"><?php _e("Get Post Idea") ?></button>
+	</form>
+
+	<div class="caption m-t-15">
+		<textarea name="topicgenerator" disabled="true" class="form-control post-message" placeholder="<?php _e('Awaiting topic input') ?>"></textarea>
+		<div class="caption-toolbar">
+			<div class="item">
+				<div class="count-word"><i class="fas fa-text-width"></i> <span>0</span></div>
+			</div>
+		</div>
+	</div>
 </div>
