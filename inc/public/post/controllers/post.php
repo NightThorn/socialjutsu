088826dec725e47a4e42794ecf43d709c2778926
greasygamerdotcom <greasygamer@greasygamer.com>
2022-04-27 11:49:56 -0500

    error 200

--- inc/public/post/controllers/post.php
+++ inc/public/post/controllers/post.php
@@ -122,7 +122,7 @@ class post extends MY_Controller
 		$status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
 
 		if (
-			$status != 201
+			$status != 201 || $status != 200
 		) {
 			die("Error: call to URL $url failed with status $status, response $json_response, curl_error " . curl_error($curl) . ", curl_errno " . curl_errno($curl));
 		}
