e8a45ee13ba255b4b0d9cf1a061a28a0196d1f87
greasygamerdotcom <greasygamer@greasygamer.com>
2022-04-27 12:50:38 -0500

    ai ideas

--- inc/public/post/views/pages/idea.php
+++ inc/public/post/views/pages/idea.php
@@ -2,52 +2,54 @@
      <div class="fm-action text-center">
 
          OR
-</div>
-         <div></div>
-         <h3>Get Post Idea</h3>
-         <form id="ideaform" method="post">
+     </div>
+     <div></div>
+     <div></div>
+     <div class="title fs-16 text-info"><i class="fa-solid fa-robot"></i> Let our A.I. Get You Some Post Ideas</div>
+     <h3></h3>
+     <form id="ideaform" method="post">
 
-             <div class="form-group">
-                 <input class="form-control" type="text" id="topic" name="topic" placeholder="<?php _e("Enter a topic") ?>">
-                 <input type="hidden" name="<?php echo $this->security->get_csrf_token_name(); ?>" value="<?php echo $this->security->get_csrf_hash(); ?>">
+         <div class="form-group">
+             <input class="form-control" type="text" id="topic" name="topic" placeholder="<?php _e("Enter a topic") ?>">
+             <input type="hidden" name="<?php echo $this->security->get_csrf_token_name(); ?>" value="<?php echo $this->security->get_csrf_hash(); ?>">
 
-             </div>
-             <div class="fm-action text-center">
-                 <button id="generate" class="btn btn-info btn-post-now" type="submit"><?php _e("Generate") ?></button>
-             </div>
-         </form>
-         <div class="caption m-t-15">
-             <textarea id="ideagenerator" disabled="true" style="height: 400px; width: 100%;"></textarea>
-             <div class="caption-toolbar" style="display: flex;">
-                 <div class="item" style="padding: 7px;">
-                     <div class="count-word"><i class="fas fa-text-width"></i> <span>0</span></div>
-                 </div>
+         </div>
+         <div class="fm-action text-center">
+             <button id="generate" class="btn btn-info btn-post-now" type="submit"><?php _e("Generate") ?></button>
+         </div>
+     </form>
+     <div class="caption m-t-15">
+         <textarea id="ideagenerator" disabled="true" style="height: 400px; width: 100%;"></textarea>
+         <div class="caption-toolbar" style="display: flex;">
+             <div class="item" style="padding: 7px;">
+                 <div class="count-word"><i class="fas fa-text-width"></i> <span>0</span></div>
              </div>
          </div>
      </div>
+ </div>
 
 
-     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 
-     <script type="text/javascript">
-         $("#generate").click(function(e) {
-             e.preventDefault();
-             var topic = $("#topic").val();
-             $.ajax({
-                 url: "<?php echo site_url('post/topic'); ?>",
-                 method: "POST",
-                 data: {
-                     topic: topic
-                 },
-                 success: function(data) {
-                     $("#ideagenerator").val(data);
-                     $("#ideagenerator").css("display", "block");
+ <script type="text/javascript">
+     $("#generate").click(function(e) {
+         e.preventDefault();
+         var topic = $("#topic").val();
+         $.ajax({
+             url: "<?php echo site_url('post/topic'); ?>",
+             method: "POST",
+             data: {
+                 topic: topic
+             },
+             success: function(data) {
+                 $("#ideagenerator").val(data);
+                 $("#ideagenerator").css("display", "block");
 
 
-                 },
-                 error: function() {
-                     alert("Something went wrong. Please try again later.");
-                 }
-             });
+             },
+             error: function() {
+                 alert("Something went wrong. Please try again later.");
+             }
          });
-     </script>
+     });
+ </script>
