8b5e73f5754c75ede75e0e62d8240941b67eed87
greasygamerdotcom <greasygamer@greasygamer.com>
2022-04-27 09:36:30 -0500

    test?

--- inc/public/post/controllers/post.php
+++ inc/public/post/controllers/post.php
@@ -85,7 +85,46 @@ class post extends MY_Controller
 	{
 
 		$topic = $_POST['topic'];
-		$this->model->idea($topic);
+		$getkey = $this->db->select('apikey')->from('apikeys')->where('id', '1')->limit(1)->get()->row();
+		$apikey =  $getkey->key;
+		$url = "https://api.openai.com/v1/engines/text-davinci-002/completions";
+
+		$data = array(
+			"prompt" => "Write a post for" + $topic,
+			"temperature" => 0.83,
+			"max_tokens" => 483,
+			"top_p" => 1,
+			"frequency_penalty" => 0,
+			"presence_penalty" => 2
+		);
+		$data_string = json_encode($data);
+
+		$curl = curl_init($url);
+		curl_setopt($curl, CURLOPT_HEADER, false);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt(
+			$curl,
+			CURLOPT_HTTPHEADER,
+			array("Content-type: application/json", $apikey)
+		);
+		curl_setopt($curl, CURLOPT_POST, true);
+		curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
+
+		$json_response = curl_exec($curl);
+
+		$status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+		if (
+			$status != 201
+		) {
+			die("Error: call to URL $url failed with status $status, response $json_response, curl_error " . curl_error($curl) . ", curl_errno " . curl_errno($curl));
+		}
+
+
+		curl_close($curl);
+
+		$response = json_decode($json_response, true);
+		return $response;
 
 	}
 
--- inc/public/post/views/pages/idea.php
+++ inc/public/post/views/pages/idea.php
@@ -1,21 +1,45 @@
-<div><div></div>
-    <h3>Get Post Idea</h3>
-    <form class="actionLogin" action="<?php _e(get_module_url('topic', $this)) ?>" method="post">
+ <div>
+     <div></div>
+     <h3>Get Post Idea</h3>
+     <form id="ideaform" method="post">
 
-        <div class="form-group">
-            <input class="form-control" type="text" id="topic" name="topic" placeholder="<?php _e("Enter a topic") ?>">
-            <input type="hidden" name="<?php echo $this->security->get_csrf_token_name(); ?>" value="<?php echo $this->security->get_csrf_hash(); ?>">
+         <div class="form-group">
+             <input class="form-control" type="text" id="topic" name="topic" placeholder="<?php _e("Enter a topic") ?>">
+             <input type="hidden" name="<?php echo $this->security->get_csrf_token_name(); ?>" value="<?php echo $this->security->get_csrf_hash(); ?>">
 
-        </div>
-        <button type="submit"><?php _e("Generate") ?></button>
+         </div>
+         <button id="generate" type="submit"><?php _e("Generate") ?></button>
 
-    </form>
-    <div class="caption m-t-15">
-        <textarea id="ideagenerator" disabled="true" class="form-control post-message"></textarea>
-        <div class="caption-toolbar">
-            <div class="item">
-                <div class="count-word"><i class="fas fa-text-width"></i> <span>0</span></div>
-            </div>
-        </div>
-    </div>
-</div>
+     </form>
+     <div class="caption m-t-15">
+         <textarea id="ideagenerator" disabled="true" class="form-control post-message"></textarea>
+         <div class="caption-toolbar">
+             <div class="item">
+                 <div class="count-word"><i class="fas fa-text-width"></i> <span>0</span></div>
+             </div>
+         </div>
+     </div>
+ </div>
+
+
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+ <script type="text/javascript">
+     $("#generate").click(function(e) {
+         e.preventDefault();
+         var topic = $("topic").val();
+         $.ajax({
+             url: "<?php echo site_url('post/topic'); ?>",
+             method: "POST",
+             data: {
+                 topic: topic
+             },
+             success: function(data) {
+                 $("#ideagenerator").html(data);
+             },
+             error: function() {
+                 alert("oof");
+             }
+         });
+     });
+ </script>
