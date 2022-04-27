b90bd34a8a5ae3c5ba5314b3ed923d9177e85cd6
greasygamerdotcom <greasygamer@greasygamer.com>
2022-04-27 11:33:02 -0500

    asdf

--- inc/public/post/controllers/post.php
+++ inc/public/post/controllers/post.php
@@ -93,14 +93,14 @@ class post extends MY_Controller
 			->row()
 			->apikey;
 		$url = "https://api.openai.com/v1/engines/text-davinci-002/completions";
-		$data = [
-			"prompt" => "Write a post for" + $topic,
+		$data = array(
+			'prompt' => 'Write a post for' + $topic,
 			'temperature' => 0.83,
 			'max_tokens' => 483,
 			'top_p' => 1,
 			'frequency_penalty' => 0,
 			'presence_penalty' => 2
-		];
+		);
 		$data_string = addslashes(json_encode($data));
 		echo $data_string;
 
