b68cb7faf517fc42b36737682a21ce8253016075
greasygamerdotcom <greasygamer@greasygamer.com>
2022-05-03 17:28:51 -0500

    fixed config db pass

--- app/config.php
+++ app/config.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_HOST', 'localhost');
 define('DB_USER', 'socialju');
-define('DB_PASS', 'idoz8cz3oq$e%u');
+define('DB_PASS', 'Deadpool98!');
 define('DB_NAME', 'socialju_mysql');
 define('TIMEZONE', 'America/Chicago');
 define('ENCRYPTION_KEY', '26a4289b130ea2632a201bd3373b366f');
