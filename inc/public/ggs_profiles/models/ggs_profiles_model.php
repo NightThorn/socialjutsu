1a2f20c0b1ec448f041e16ac2125475617a24fa1
greasygamerdotcom <greasygamer@greasygamer.com>
2022-05-05 10:56:53 -0500

    config path

--- inc/public/ggs_profiles/models/ggs_profiles_model.php
+++ inc/public/ggs_profiles/models/ggs_profiles_model.php
@@ -11,6 +11,8 @@ class ggs_profiles_model extends MY_Model {
 		$this->module_name = get_module_config( $module_path, 'name' );
 		$this->module_icon = get_module_config( $module_path, 'icon' );
 		$this->module_color = get_module_config( $module_path, 'color' );
+		$this->module_img = get_module_config($module_path, 'img');
+
 		//
 	}
 
