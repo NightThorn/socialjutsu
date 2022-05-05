f8902c0f5f2b4a31623ef493d35a331a200768b5
greasygamerdotcom <greasygamer@greasygamer.com>
2022-05-05 16:12:42 -0500

    ggs

---
+++ inc/public/ggs_post/config.php
@@ -0,0 +1,25 @@
+<?php
+return [
+    'id' => 'ggs_post',
+    'name' => 'GGs Post',
+    'author' => '',
+    'author_uri' => 'https://socialjutsu.com',
+    'version' => '1.0',
+    'desc' => '',
+    'icon' => '',
+    'img' => 'https://ggspace.nyc3.cdn.digitaloceanspaces.com/uploads/photos/2022/04/gg_a542a716a5e431dbeee72402d1fcdb0b.png',
+    'color' => '#00FF00',
+    'menu' => [
+        'tab' => 2,
+        'position' => 1000,
+        'name' => 'GGs',
+        'sub_menu' => [
+        	'position' => 1000,
+            'id' => 'ggs_post',
+            'name' => 'Post'
+        ]
+    ],
+    'css' => [
+		'assets/css/ggs_post.css'
+	]
+];
---
+++ inc/public/ggs_post/language/ggs_post_lang.php
@@ -0,0 +1,15 @@
+<?php
+$lang["GGs Post"] = "GGs Post";
+$lang["Post"] = "Post";
+$lang["Media"] = "Media";
+$lang["Link"] = "Link";
+$lang["Text"] = "Text";
+$lang["Last 30 days"] = "Last 30 days";
+$lang["Successful"] = "Successful";
+$lang["Failed"] = "Failed";
+$lang["Post type"] = "Post type";
+$lang["Total"] = "Total";
+$lang["Anonymous"] = "Anonymous";
+$lang["Like"] = "Like";
+$lang["Comment"] = "Comment";
+$lang["Share"] = "Share";
