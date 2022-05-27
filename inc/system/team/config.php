35705e9ddb8aeb7bd4d5c82c3d99692e92dd32e1
greasygamerdotcom <greasygamer@greasygamer.com>
2022-05-27 11:37:28 -0500

    team added

---
+++ inc/system/team/config.php
@@ -0,0 +1,20 @@
+<?php
+return [
+    'id' => 'team',
+    'name' => 'Team Manager',
+    'author' => 'SocialJutsu',
+    'author_uri' => 'https://socialjutsu.com',
+    'version' => '1.0',
+    'desc' => '',
+    'icon' => 'fas fa-users',
+    'color' => '#05ebda',
+    'pricing' => true,
+    'menu' => [
+    	'tab' => 3,
+    	'position' => 4000,
+    	'name' => 'Team Manager'
+    ],
+    'css' => [
+        'assets/css/team.css'
+    ]
+];
