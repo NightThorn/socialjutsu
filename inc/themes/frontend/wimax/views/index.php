535ffb0f6aaafb6ddafdfab6aaf08b7bde2185fc
Austin Hayes <greasygamerforums@gmail.com>
2022-07-15 18:44:16 -0500

    changed words

--- inc/themes/frontend/wimax/views/index.php
+++ inc/themes/frontend/wimax/views/index.php
@@ -26,7 +26,7 @@
         <div class="welcome_text_area">
           <h2 class="wow fadeInUp" data-wow-delay="0.2s"><?php _e("SocialJutsu™") ?></h2>
           <h3 class="wow fadeInUp" style="color: grey;" data-wow-delay="0.2s">Social Media Marketing Made Easy.</h3>
-          <h5 class="wow fadeInUp" style="color: grey;" data-wow-delay="0.2s">We handle your digital marketing, so you don't have too! We specialize in Facebook ads, promoting your business to over <b>3 Billion</b> daily active Facebook users! We can target your exact audience, maximizing the returns on your ad budget.</h5>
+          <h5 class="wow fadeInUp" style="color: grey;" data-wow-delay="0.2s">We handle your social media digital marketing, so you don't have too! We specialize in Facebook ads, promoting your business to over <b>3 Billion</b> daily active Facebook users! We can target your exact audience, maximizing the returns on your ad budget.</h5>
 
           <!--<a class="btn wimax-btn mt-30 wow fadeInUp" href="<?php _e(get_url("signup")) ?>" data-wow-delay="0.4s"><?php _e("Join the Beta!") ?></a><a class="btn wimax-btn btn-2 mt-30 ml-2 wow fadeInUp" href="#features" data-wow-delay="0.5s"><?php _e("Learn More") ?></a>
 -->
