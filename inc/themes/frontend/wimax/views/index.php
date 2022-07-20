c14577e8bd6d4f71410ea52837c008485c47418c
greasygamerdotcom <greasygamer@greasygamer.com>
2022-07-20 10:36:43 -0500

    fixed style of index

--- inc/themes/frontend/wimax/views/index.php
+++ inc/themes/frontend/wimax/views/index.php
@@ -12,20 +12,31 @@
 
   }
 
+  .media img {
+
+    width: 100%;
+    height: 15vw;
+    object-fit: contain;
+  }
+
+
+
   .card-header {
     border-radius: 20px !important;
     background-color: #37c30d;
   }
 
-  .card {
+  .carditem {
     border-radius: 20px;
+    padding: 16px;
+    text-align: center;
     border: none;
     box-shadow: 1px 0px 47px -1px rgba(0, 0, 0, 0.75);
     -webkit-box-shadow: 1px 0px 47px -1px rgba(0, 0, 0, 0.75);
     -moz-box-shadow: 1px 0px 47px -1px rgba(0, 0, 0, 0.75);
   }
 
-  .card:hover {
+  .carditem:hover {
 
     cursor: pointer;
     box-shadow: none;
@@ -35,19 +46,55 @@
 
     padding: 20px;
   }
+
+  .col {
+    float: left;
+    width: 25%;
+    padding: 0 10px;
+  }
+
+  /* Remove extra left and right margins, due to padding */
+  .rowcards {
+    margin: 0 -5px;
+  }
+
+  /* Clear floats after the columns */
+  .rowcards:after {
+    content: "";
+    display: table;
+    clear: both;
+  }
+
+  .card-body {
+
+    overflow-wrap: break-word;
+
+  }
+
+  /* Responsive columns */
+  @media screen and (max-width: 900px) {
+    .col {
+      width: 100%;
+      display: block;
+      margin-bottom: 20px;
+    }
+  }
+
+  /* Style the counter cards */
 </style>
 <section class="hero-barishal welcome_area" id="recent">
 
   <div class="container h-50">
-    <div class="row h-100 justify-content-between align-items-center">
-      <div class="heading">
+    <div class="heading">
 
-        <h3>Latest Finds</h3>
-      </div>
-      <div class="col-12" style="flex-wrap: wrap; display: flex; justify-content: space-between; height: 50%;">
-        <?php foreach ($latest as $key => $item) : ?>
+      <h3>Latest Finds</h3>
+    </div>
+    <div class="rowcards">
+
+      <?php foreach ($latest as $key => $item) : ?>
+        <div class="col">
           <a href="<?php _e($item->link) ?>" target="_blank">
-            <div class="card" style="margin-bottom: 20px; min-width: 45%; max-width: 500px;">
+            <div class="carditem">
               <div class="card-header">
                 <h6><?php _e($item->title) ?></h6>
               </div>
@@ -60,8 +107,8 @@
 
             </div>
           </a>
-        <?php endforeach ?>
-      </div>
+        </div>
+      <?php endforeach ?>
     </div>
 
   </div>
@@ -72,16 +119,16 @@
 <section class="hero-barishal welcome_area" id="home">
 
   <div class="container h-50">
-    <div class="row h-100 justify-content-between align-items-center">
-      <div class="heading">
+    <div class="heading">
 
-        <h3>Home</h3>
-      </div>
-      <div class="col-12" style="flex-wrap: wrap; display: flex; justify-content: space-between; height: 50%;">
-        <?php foreach ($items as $key => $item) : ?>
+      <h3>Home</h3>
+    </div>
+    <div class="rowcards">
+
+      <?php foreach ($items as $key => $item) : ?>
+        <div class="col">
           <a href="<?php _e($item->link) ?>" target="_blank">
-
-            <div class="card" style="margin-bottom: 20px; min-width: 45%; max-width: 500px;">
+            <div class="carditem">
               <div class="card-header">
                 <h6><?php _e($item->title) ?></h6>
               </div>
@@ -94,11 +141,9 @@
 
             </div>
           </a>
-        <?php endforeach ?>
-      </div>
+        </div>
+      <?php endforeach ?>
     </div>
-
-  </div>
 
 
 
@@ -107,16 +152,16 @@
 <section class="hero-barishal welcome_area" id="beauty">
 
   <div class="container h-50">
-    <div class="row h-100 justify-content-between align-items-center">
-      <div class="heading">
+    <div class="heading">
 
-        <h3>Beauty</h3>
-      </div>
-      <div class="col-12" style="flex-wrap: wrap; display: flex; justify-content: space-between; height: 50%;">
-        <?php foreach ($beauty as $key => $item) : ?>
+      <h3>Beauty</h3>
+    </div>
+    <div class="rowcards">
+
+      <?php foreach ($beauty as $key => $item) : ?>
+        <div class="col">
           <a href="<?php _e($item->link) ?>" target="_blank">
-
-            <div class="card" style="margin-bottom: 20px; min-width: 45%; max-width: 500px;">
+            <div class="carditem">
               <div class="card-header">
                 <h6><?php _e($item->title) ?></h6>
               </div>
@@ -129,11 +174,9 @@
 
             </div>
           </a>
-        <?php endforeach ?>
-      </div>
+        </div>
+      <?php endforeach ?>
     </div>
-
-  </div>
 
 
 
@@ -141,16 +184,16 @@
 <section class="hero-barishal welcome_area" id="tech">
 
   <div class="container h-50">
-    <div class="row h-100 justify-content-between align-items-center">
-      <div class="heading">
+    <div class="heading">
 
-        <h3>Tech</h3>
-      </div>
-      <div class="col-12" style="flex-wrap: wrap; display: flex; justify-content: space-between; height: 50%;">
-        <?php foreach ($tech as $key => $item) : ?>
+      <h3>Tech</h3>
+    </div>
+    <div class="rowcards">
+
+      <?php foreach ($tech as $key => $item) : ?>
+        <div class="col">
           <a href="<?php _e($item->link) ?>" target="_blank">
-
-            <div class="card" style="margin-bottom: 20px; min-width: 45%; max-width: 500px;">
+            <div class="carditem">
               <div class="card-header">
                 <h6><?php _e($item->title) ?></h6>
               </div>
@@ -163,28 +206,25 @@
 
             </div>
           </a>
-        <?php endforeach ?>
-      </div>
+        </div>
+      <?php endforeach ?>
     </div>
-
-  </div>
 
 
 
 </section>
 <section class="hero-barishal welcome_area" id="tools">
-
   <div class="container h-50">
-    <div class="row h-100 justify-content-between align-items-center">
-      <div class="heading">
+    <div class="heading">
 
-        <h3>Tools</h3>
-      </div>
-      <div class="col-12" style="flex-wrap: wrap; display: flex; justify-content: space-between; height: 50%;">
-        <?php foreach ($tools as $key => $item) : ?>
+      <h3>Tools and Gadgets</h3>
+    </div>
+    <div class="rowcards">
+
+      <?php foreach ($tools as $key => $item) : ?>
+        <div class="col">
           <a href="<?php _e($item->link) ?>" target="_blank">
-
-            <div class="card" style="margin-bottom: 20px; min-width: 45%; max-width: 500px;">
+            <div class="carditem">
               <div class="card-header">
                 <h6><?php _e($item->title) ?></h6>
               </div>
@@ -197,11 +237,9 @@
 
             </div>
           </a>
-        <?php endforeach ?>
-      </div>
+        </div>
+      <?php endforeach ?>
     </div>
-
-  </div>
 
 
 
@@ -210,15 +248,16 @@
 <section class="hero-barishal welcome_area" id="pets">
 
   <div class="container h-50">
-    <div class="row h-100 justify-content-between align-items-center">
-      <div class="heading">
+    <div class="heading">
 
-        <h3>Pets</h3>
-      </div>
-      <div class="col-12" style="flex-wrap: wrap; display: flex; justify-content: space-between; height: 50%;">
-        <?php foreach ($pets as $key => $item) : ?>
+      <h3>Pets</h3>
+    </div>
+    <div class="rowcards">
+
+      <?php foreach ($pets as $key => $item) : ?>
+        <div class="col">
           <a href="<?php _e($item->link) ?>" target="_blank">
-            <div class="card" style="margin-bottom: 20px; min-width: 45%; max-width: 500px;">
+            <div class="carditem">
               <div class="card-header">
                 <h6><?php _e($item->title) ?></h6>
               </div>
@@ -231,27 +270,25 @@
 
             </div>
           </a>
-        <?php endforeach ?>
-      </div>
+        </div>
+      <?php endforeach ?>
     </div>
-
-  </div>
 
 
 
 </section>
 <section class="hero-barishal welcome_area" id="kids">
-
   <div class="container h-50">
-    <div class="row h-100 justify-content-between align-items-center">
-      <div class="heading">
+    <div class="heading">
 
-        <h3>Kids</h3>
-      </div>
-      <div class="col-12" style="flex-wrap: wrap; display: flex; justify-content: space-between; height: 50%;">
-        <?php foreach ($kids as $key => $item) : ?>
+      <h3>Kids/Baby</h3>
+    </div>
+    <div class="rowcards">
+
+      <?php foreach ($kids as $key => $item) : ?>
+        <div class="col">
           <a href="<?php _e($item->link) ?>" target="_blank">
-            <div class="card" style="margin-bottom: 20px; min-width: 45%; max-width: 500px;">
+            <div class="carditem">
               <div class="card-header">
                 <h6><?php _e($item->title) ?></h6>
               </div>
@@ -264,27 +301,25 @@
 
             </div>
           </a>
-        <?php endforeach ?>
-      </div>
+        </div>
+      <?php endforeach ?>
     </div>
-
-  </div>
 
 
 
 </section>
 <section class="hero-barishal welcome_area" id="outdoors">
-
   <div class="container h-50">
-    <div class="row h-100 justify-content-between align-items-center">
-      <div class="heading">
+    <div class="heading">
 
-        <h3>Sports/Outdoors</h3>
-      </div>
-      <div class="col-12" style="flex-wrap: wrap; display: flex; justify-content: space-between; height: 50%;">
-        <?php foreach ($outdoors as $key => $item) : ?>
+      <h3>Sports/Outdoors</h3>
+    </div>
+    <div class="rowcards">
+
+      <?php foreach ($outdoors as $key => $item) : ?>
+        <div class="col">
           <a href="<?php _e($item->link) ?>" target="_blank">
-            <div class="card" style="margin-bottom: 20px; min-width: 45%; max-width: 500px;">
+            <div class="carditem">
               <div class="card-header">
                 <h6><?php _e($item->title) ?></h6>
               </div>
@@ -297,11 +332,9 @@
 
             </div>
           </a>
-        <?php endforeach ?>
-      </div>
+        </div>
+      <?php endforeach ?>
     </div>
-
-  </div>
 
 
 
@@ -309,15 +342,16 @@
 <section class="hero-barishal welcome_area" id="kitchen">
 
   <div class="container h-50">
-    <div class="row h-100 justify-content-between align-items-center">
-      <div class="heading">
+    <div class="heading">
 
-        <h3>Kitchen</h3>
-      </div>
-      <div class="col-12" style="flex-wrap: wrap; display: flex; justify-content: space-between; height: 50%;">
-        <?php foreach ($kitchen as $key => $item) : ?>
+      <h3>Kitchen</h3>
+    </div>
+    <div class="rowcards">
+
+      <?php foreach ($kitchen as $key => $item) : ?>
+        <div class="col">
           <a href="<?php _e($item->link) ?>" target="_blank">
-            <div class="card" style="margin-bottom: 20px; min-width: 45%; max-width: 500px;">
+            <div class="carditem">
               <div class="card-header">
                 <h6><?php _e($item->title) ?></h6>
               </div>
@@ -330,12 +364,9 @@
 
             </div>
           </a>
-        <?php endforeach ?>
-      </div>
+        </div>
+      <?php endforeach ?>
     </div>
-
-  </div>
-
 
 
 </section>
